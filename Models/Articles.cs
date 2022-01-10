d327b7a09aab7031cb15288a898dc6c65747f82b
BigInvoker <ghost.task.quest@gmail.com>
2022-01-10 09:34:20 +0330

    Move Info part of index page to a partial-view named Info.cshtml

---
+++ Models/Articles.cs
@@ -0,0 +1,10 @@
+namespace Portfolio.Models
+{
+    public class Articles
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImageAddress { get; set; }
+    }
+}
---
+++ Models/Info.cs
@@ -0,0 +1,12 @@
+
+
+namespace Portfolio.Models
+{
+    public class Info
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImageAddress { get; set; }
+    }
+}
